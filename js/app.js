04f8651e7d9e5d13deaa909ddd4afd010c793a1a
Matan Uchen <matan.uchen@gmail.com>
2016-09-04 09:15:58 -0400

    Added rules.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,16 @@
 (function() {
-    var app = angular.module('brooklynStratTFL', ['playersModule', 'pairingsModule']);
+    var app = angular.module('brooklynStratTFL', ['playersModule', 'pairingsModule', 'ngDialog']);
 
-    app.controller('MainController', ['$scope', function($scope) {
-
+    app.controller('MainController', ['$scope', 'ngDialog', function($scope, ngDialog) {
+        $scope.showRules = function() {
+            ngDialog.open({ template: 'templates/rules.html', className: 'ngdialog-theme-plain', scope: $scope });
+        };
     }]);
+
+    app.directive('rules', function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/rules.html'
+        };
+    });
 })();
